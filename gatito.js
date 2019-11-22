09ed3bab07e95efb0d595e42202712d284ff60b0
GerardoDM <geradelgado22@hotmail.com>
2019-11-22 09:49:33 -0600

    actualizado

--- gatito.js
+++ gatito.js
@@ -1,14 +1,24 @@
 
+
+matrix = [[ '','',''],
+          ['','','',],
+          ['','','' ]];
+
+
+$( "#card" ).click(function() {
+    $( "#card" ).animate({
+      opacity: 0.25,
+      left: "+=50",
+      height: "toggle"
+    }, 5000, function() {
+      // Animation complete.
+    });
+  });
+
 var turno = "jugador1";
 
-$(".cuadro").click(function() {
-	
-	// if ($("#1").text()!="1") {
-    
-    //     alert("si jala");
-        
-    //      }
-         
+$(".col").click(function() {
+	         
 	if (turno=="jugador1")
 	{
 		$(this).html("o");
@@ -27,38 +37,38 @@ $(".cuadro").click(function() {
         // console.log(this.getAttribute("data-value"));
     }
     
-    if ($("#1").attr('data-value') == 'x' && ($("#2").attr('data-value') == 'x') && $("#3").attr('data-value') == 'x') {
-        alert("Ganaste");
+   if (($("#11").attr('data-value') == 'x' && ($("#22").attr('data-value') == 'x') && $("#33").attr('data-value') == 'x')
+    || ($("#11").attr('data-value') == 'x' && ($("#44").attr('data-value') == 'x') && $("#77").attr('data-value') == 'x')
+    || ($("#22").attr('data-value') == 'x' && ($("#55").attr('data-value') == 'x') && $("#88").attr('data-value') == 'x')
+    || ($("#33").attr('data-value') == 'x' && ($("#66").attr('data-value') == 'x') && $("#99").attr('data-value') == 'x')
+    || ($("#44").attr('data-value') == 'x' && ($("#55").attr('data-value') == 'x') && $("#66").attr('data-value') == 'x')
+    || ($("#77").attr('data-value') == 'x' && ($("#88").attr('data-value') == 'x') && $("#99").attr('data-value') == 'x')
+    || ($("#11").attr('data-value') == 'x' && ($("#55").attr('data-value') == 'x') && $("#99").attr('data-value') == 'x')
+    || ($("#33").attr('data-value') == 'x' && ($("#55").attr('data-value') == 'x') && $("#77").attr('data-value') == 'x')) {
+         alert("Ganaste , X");
+         $('.col').html("");
+         $('.col').attr("data-value", "0");
      }
 
-    //  if ($("#1").attr('data-value') == 'x' && ($("#4").attr('data-value') == 'x') && $("#7").attr('data-value') == 'x') {
-    //     alert("Ganaste");
-    //  }
-
-    //  if ($("#1").attr('data-value') == 'x' && ($("#2").attr('data-value') == 'x') && $("#3").attr('data-value') == 'x') {
-    //     alert("Ganaste");
-    //  }
-
-    //  if ($("#1").attr('data-value') == 'x' && ($("#2").attr('data-value') == 'x') && $("#3").attr('data-value') == 'x') {
-    //     alert("Ganaste");
-    //  }
-
-     
-
-
+     if (($("#11").attr('data-value') == 'o' && ($("#22").attr('data-value') == 'o') && $("#33").attr('data-value') == 'o')
+     || ($("#11").attr('data-value') == 'o' && ($("#44").attr('data-value') == 'o') && $("#77").attr('data-value') == 'o')
+     || ($("#22").attr('data-value') == 'o' && ($("#55").attr('data-value') == 'o') && $("#88").attr('data-value') == 'o')
+     || ($("#33").attr('data-value') == 'o' && ($("#66").attr('data-value') == 'o') && $("#99").attr('data-value') == 'o')
+     || ($("#44").attr('data-value') == 'o' && ($("#55").attr('data-value') == 'o') && $("#66").attr('data-value') == 'o')
+     || ($("#77").attr('data-value') == 'o' && ($("#88").attr('data-value') == 'o') && $("#99").attr('data-value') == 'o')
+     || ($("#11").attr('data-value') == 'o' && ($("#55").attr('data-value') == 'o') && $("#99").attr('data-value') == 'o')
+     || ($("#33").attr('data-value') == 'o' && ($("#55").attr('data-value') == 'o') && $("#77").attr('data-value') == 'o')) {
+          alert("Ganaste , O");
+          $('.col').html("");
+          $('.col').attr("data-value", "0");
+      }
 
 });
 
-
-
-
-    
-  
-
-
-
 $("#limpiar").click(function() {
 
+    $('.col').html("");
+    $('.col').attr("data-value", "0");
 
 
 });
